aa7e205d9be1fadb5f930cea12bdc0c4a35cc699
EdwardEric <airburst30@gmail.com>
2017-01-19 03:26:44 +0800

    進行第一次的重構，將優惠獨立出來成funciton

--- ShoppingCart/ShoppingCart/Class1.cs
+++ ShoppingCart/ShoppingCart/Class1.cs
@@ -17,20 +17,39 @@ namespace ShoppingCart
         public double CalculateSumPrice(List<Book> bookGroup)
         {
             double result = 0;
+            double preferential = GetPreferential(bookGroup.Count);
             foreach (var book in bookGroup)
             {
                 result += book.Price * book.Count;
             }
+            result *= preferential;
 
-            if (bookGroup.Count == 2)
-                result *= 0.95;
-            if (bookGroup.Count == 3)
-                result *= 0.90;
-            if (bookGroup.Count == 4)
-                result *= 0.80;
-            if (bookGroup.Count == 5)
-                result *= 0.75;
             return result;
+        }
+        /// <summary>
+        /// 取得優惠
+        /// </summary>
+        /// <param name="count"></param>
+        /// <returns></returns>
+        private double GetPreferential(int count)
+        {
+            double preferential = 1;
+            switch (count)
+            {
+                case 2:
+                    preferential = 0.95;
+                    break;
+                case 3:
+                    preferential = 0.90;
+                    break;
+                case 4:
+                    preferential = 0.80;
+                    break;
+                case 5:
+                    preferential = 0.75;
+                    break;
+            }
+            return preferential;
         }
     }
 }
